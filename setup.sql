808a8ab5d7970859f26116f4035305b47e451fce
KeeganObonyo <keeganobonyo@gmail.com>
2018-09-21 19:42:44 +0300

    setup.sql

---
+++ setup.sql
@@ -0,0 +1,9 @@
+drop table users
+create table users
+
+create table users (
+  id         serial primary key,
+  name       varchar(255),
+  age        int
+  countryofresidence varchar(255) not null,
+);
