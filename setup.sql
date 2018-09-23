5662d40c3a8011441208286ca29aed07a910e436
KeeganObonyo <keeganobonyo@gmail.com>
2018-09-23 19:12:16 +0300

    minor fix

--- setup.sql
+++ setup.sql
@@ -1,9 +1,8 @@
-drop table users
-create table users
+drop table users;
 
 create table users (
   id         serial primary key,
   name       varchar(255),
-  age        int
-  countryofresidence varchar(255) not null,
+  age        int,
+  countryofresidence varchar(255) not null
 );
