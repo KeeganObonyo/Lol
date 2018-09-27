97748e878b5b8f0f5f41d80c855e20eb0ebc51ec
KeeganObonyo <keeganobonyo@gmail.com>
2018-09-27 21:21:24 +0300

    provision for password hashing and log in added

--- setup.sql
+++ setup.sql
@@ -1,8 +1,11 @@
+create extension pgcrypto;
+
 drop table users;
 
 create table users (
   id         serial primary key,
+  -- uuid       uuid not null default gen_random_uuid(),
   name       varchar(255),
-  age        int,
-  countryofresidence varchar(255) not null
+  email      varchar(255) not null unique,
+  password varchar(255) not null
 );
