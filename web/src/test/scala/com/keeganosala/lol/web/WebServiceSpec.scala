da794b2033e0fa5b748bd6de623dc38c109cfe24
KeeganObonyo <keeganobonyo@gmail.com>
2019-01-08 16:33:27 +0300

    tests for the web service added

---
+++ web/src/test/scala/com/keeganosala/lol/web/WebServiceSpec.scala
@@ -0,0 +1,102 @@
+package com.keeganosala.lol.web
+
+import scala.concurrent.duration._
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.testkit.{ RouteTestTimeout, ScalatestRouteTest }
+import akka.http.scaladsl.server._
+import Directives._
+
+import spray.json._
+import DefaultJsonProtocol._
+
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{ Matchers, WordSpec }
+
+import com.keeganosala._
+
+import lol.core.db.postgres.service.PostgresDbService._
+
+import lol.core.util._
+
+import auth._
+
+class WebServiceSpec extends WordSpec 
+  	with Matchers 
+  	with ScalaFutures 
+	  with ScalatestRouteTest
+    with WebService     
+    with LolJsonProtocol
+    with AuthenticationLogic {
+
+  lazy val routes               = route
+
+  implicit val routeTestTimeout = RouteTestTimeout(FiniteDuration(20, "seconds"))
+  
+  val invalidAuthToken          = "ATtkn_InvalidAuthToken"
+
+  val validAuthToken            = retrieveToken(UserDbRetrieveServiceResponse(
+      id        =1,
+      name      = "TestUSer",
+      email     = "test@gmail.com",
+      password  ="dfghjkl;kl;;''"
+  ))
+
+  "LolWebService" should {
+    "Reject a request if the user sends in an invalid token" in {
+      Get("/lol/data/get") ~> addHeader("Authorization", invalidAuthToken) ~> Route.seal(route) ~> check {
+        status shouldEqual StatusCodes.Unauthorized
+      }
+    }
+    "be able to return a slice of market data" in {
+      Get("/lol/data/get") ~> addHeader("Authorization", validAuthToken) ~> Route.seal(route) ~> check {
+        status shouldEqual StatusCodes.OK
+      }
+    }
+    "be able to return analysed market data" in {
+      Get("/lol/data/compute") ~> addHeader("Authorization", validAuthToken) ~> Route.seal(route) ~> check {
+        status shouldEqual StatusCodes.OK
+      }
+    }
+    "be able to return a list of users" in {
+      Get("/lol/users") ~> addHeader("Authorization", validAuthToken) ~> Route.seal(route) ~> check {
+        status shouldEqual StatusCodes.OK
+      }
+    }
+    "be able to return a single user" in {
+      Get("/lol/users/1") ~> addHeader("Authorization", validAuthToken) ~> Route.seal(route) ~> check {
+        status shouldEqual StatusCodes.OK
+      }
+    }
+    "be able to add a new user" in {
+      val user = JsObject(
+        "email"    -> "user@gmail.com".toJson,
+        "name"     -> "user1".toJson,
+        "password" -> "password".toJson
+      )
+      Post("/lol/users", user) ~> routes ~> check {
+        status shouldEqual StatusCodes.Created
+      }
+    }
+    "be able to reject an invalid user registration" in {
+      val user = JsObject(
+        "name"     -> "user1".toJson,
+        "password" -> "password".toJson
+      )
+      Post("/lol/users", user) ~> Route.seal(route) ~> check {
+        status shouldEqual StatusCodes.BadRequest
+      }
+    }
+    "be able to delete a registered user" in {
+      Put("/food/order/1") ~> Route.seal(route) ~> check {
+        status shouldEqual StatusCodes.MethodNotAllowed
+        responseAs[String] shouldEqual "HTTP method not allowed, supported methods: POST"
+      }
+    }
+    "leave requests with invalid paths unhandled" in {
+      Post("lol/other") ~> route ~> check {
+        handled shouldEqual false
+      }
+    }
+  }
+}
